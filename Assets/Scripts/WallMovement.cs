a6bc9983ddb3edb01c7fbe4286e9c708adb227f9
navidAdelpour <navidadelpour@gmail.com>
2018-12-03 00:21:23 +0330

    wall movement added

---
+++ Assets/Scripts/WallMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallMovement : MonoBehaviour {
+
+	private Vector2 texture_offset;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		GetComponent<Renderer> ().material.mainTextureOffset += Vector2.left * ((int) GameManager.instance.game_speed) / 1000;
+	}
+}
