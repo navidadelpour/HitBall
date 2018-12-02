1bfed9a39070e33489b4f2f66b3e831adc842fd6
navidAdelpour <navidadelpour@gmail.com>
2018-12-02 23:40:49 +0330

    player speed and bound speed increasing with input added

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
+	public static GameManager instance;
+	public float game_speed;
+	public float max_speed;
+	public float min_speed;
+	private float bound_increase_amount;
+	private float speed_increase_amount;
+	private float speed_decrease_amount;
+
+	void Init() {
+		instance = this;
+		min_speed = 5f;
+		max_speed = 15f;
+		bound_increase_amount = .001f;
+		speed_increase_amount = .4f;
+		speed_decrease_amount = .2f;
+		game_speed = min_speed;
+	}
+
 	void Start () {
-		
+		Init ();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		// TODO: increase speed algorithm
+		min_speed += bound_increase_amount;
+		max_speed += bound_increase_amount;
 	}
+
+	public void ShouldIncreaseSpeed(bool should_increase) {
+		if (should_increase) {
+			if (game_speed < max_speed)
+				game_speed += speed_increase_amount;
+		} else {
+			if (game_speed > min_speed)
+				game_speed -= speed_decrease_amount;
+			else
+				game_speed += speed_increase_amount;
+		}
+	}
+
 }
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	public static InputManager instance;
+
+	void Init() {
+		instance = this;
+	}
+
+	void Start () {
+		Init ();
+	}
+	
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			GameManager.instance.ShouldIncreaseSpeed (true);
+		} else {
+			GameManager.instance.ShouldIncreaseSpeed (false);
+		}
+
+	}
+}
