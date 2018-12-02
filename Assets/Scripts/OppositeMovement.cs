4b3c4a92633f7630a603e33f101043469638baff
navidAdelpour <navidadelpour@gmail.com>
2018-12-02 23:02:30 +0330

    opposite movement added

---
+++ Assets/Scripts/OppositeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OppositeMovement : MonoBehaviour {
+
+	private float speed;
+	private Vector2 opposite_velocity;
+	private Rigidbody2D body;
+
+	void Init() {
+		speed = 5f;
+		opposite_velocity = Vector3.left * speed;
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start () {
+		Init ();
+	}
+	
+	void FixedUpdate () {
+		body.velocity = opposite_velocity;
+	}
+}
