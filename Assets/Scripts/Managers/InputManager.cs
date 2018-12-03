0c423dc23cabb1e3e0be984f139d16bb0793461c
navidAdelpour <navidadelpour@gmail.com>
2018-12-03 19:01:56 +0330

    trying to make input managing with raycast

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -6,6 +6,8 @@ public class InputManager : MonoBehaviour {
 
 	public static InputManager instance;
 
+	public bool can_act;
+
 	void Init() {
 		instance = this;
 	}
@@ -15,7 +17,22 @@ public class InputManager : MonoBehaviour {
 	}
 	
 	void Update () {
-		SpeedManager.instance.ShouldIncreaseSpeed (Input.GetMouseButton (0));
-		GameManager.instance.has_shield = Input.GetMouseButton(1);
+		
+		if (Input.anyKey) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				Debug.Log (hit.collider.name);
+				if (hit.collider.gameObject.layer == 5) {
+					can_act = false;
+				} else
+					can_act = true;
+			}
+		}
+
+		if (can_act) {
+			SpeedManager.instance.ShouldIncreaseSpeed (Input.GetMouseButton (0));
+			GameManager.instance.has_shield = Input.GetMouseButton (1);
+		}
 	}
 }
