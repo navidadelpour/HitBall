a4d8cee1680f194dc2c363b72de8cfaefaacc258
navidAdelpour <navidadelpour@gmail.com>
2018-12-18 22:45:21 +0330

    making jump height dynamic

--- Assets/Scripts/Managers/HeightManager.cs
+++ Assets/Scripts/Managers/HeightManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +7,11 @@ public class HeightManager : MonoBehaviour {
 	public static HeightManager self;
 
 	public float player_jump_height;
+	public float jump_increase_amount = 1f;
 	public float player_coil_jump_height = 13f;
-	private float player_max_jump_height = 10f;
+	private float player_max_jump_height = 12f;
 	private float player_normal_jump_height = 8f;
-	private float player_min_jump_height = 6f;
+	private float player_min_jump_height = 4f;
 
 	void Awake() {
 		self = this;
@@ -27,13 +28,18 @@ public class HeightManager : MonoBehaviour {
 	public void SetHeight() {
 		switch (SpeedManager.self.state) {
 		case SpeedStates.INCREASE:
-			player_jump_height = player_max_jump_height;
+            if(player_jump_height < player_max_jump_height)
+			    player_jump_height += jump_increase_amount;
 			break;
 		case SpeedStates.NORMALIZE:
-			player_jump_height = player_normal_jump_height;
+            if(player_jump_height > player_normal_jump_height)
+			    player_jump_height -= jump_increase_amount;
+            else if (player_jump_height < player_normal_jump_height)
+                player_jump_height += jump_increase_amount;
 			break;
 		case SpeedStates.DECREASE:
-			player_jump_height = player_min_jump_height;
+            if(player_jump_height > player_min_jump_height)
+			    player_jump_height -= jump_increase_amount;
 			break;
 		}
 	}
