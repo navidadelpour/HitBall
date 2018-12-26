08161d58c4c88aa2e8134ef58490fd889727f872
navidAdelpour <navidadelpour@gmail.com>
2018-12-26 18:32:53 +0330

    changing parameters a bit

--- Assets/Scripts/Managers/SpeedManager.cs
+++ Assets/Scripts/Managers/SpeedManager.cs
@@ -10,14 +10,14 @@ public class SpeedManager : MonoBehaviour {
 
 	public float game_speed;
 	private float game_normal_speed;
-	private Dictionary<string, float> game_min_speed = new Dictionary<string, float>() {{"min", 4f}, {"max", 8f}};
-	private Dictionary<string, float> game_max_speed = new Dictionary<string, float>() {{"min", 8f}, {"max", 16f}};
+	private Dictionary<string, float> game_min_speed = new Dictionary<string, float>() {{"min", 4f}};
+	private Dictionary<string, float> game_max_speed = new Dictionary<string, float>() {{"min", 10f}};
 	private float game_speed_diffrence_amount = .2f;
 
 	public float player_speed;
 	private float player_normal_speed;
-	private Dictionary<string, float> player_min_speed = new Dictionary<string, float>() {{"min", 8f}, {"max", 16f}};
-	private Dictionary<string, float> player_max_speed = new Dictionary<string, float>() {{"min", 12f}, {"max", 24f}};
+	private Dictionary<string, float> player_min_speed = new Dictionary<string, float>() {{"min", 6f}};
+	private Dictionary<string, float> player_max_speed = new Dictionary<string, float>() {{"min", 14f}};
 	private float player_speed_difference_amount = .2f;
 
 	private float game_bound_increase_amount = 0.001f; 
@@ -30,6 +30,10 @@ public class SpeedManager : MonoBehaviour {
 	void Init() {
 		game_normal_speed = (game_min_speed["min"] + game_max_speed["min"]) / 2;
 		player_normal_speed = (player_min_speed["min"] + player_max_speed["min"]) / 2;
+		game_min_speed["max"] = game_min_speed["min"] * 2;
+		game_max_speed["max"] = game_max_speed["min"] * 2;
+		player_max_speed["max"] = player_max_speed["min"] * 2;
+		player_min_speed["max"] = player_min_speed["min"] * 2;
 		game_speed = game_normal_speed;
 		player_speed = player_normal_speed;
 	}
@@ -42,41 +46,29 @@ public class SpeedManager : MonoBehaviour {
 		MakeHard();
 		switch (state) {
 		case SpeedStates.INCREASE:
-			IncreaseSpeed ();
+			if (game_speed < game_max_speed["min"])
+				game_speed += game_speed_diffrence_amount;
+			if (player_speed > player_min_speed["min"])
+				player_speed -= player_speed_difference_amount;
 			break;
 		case SpeedStates.NORMALIZE:
-			NormalizeSpeed ();
+			if (game_speed > game_normal_speed)
+				game_speed -= game_speed_diffrence_amount;
+			else 
+				game_speed += game_speed_diffrence_amount;
+
+			if (player_speed > player_normal_speed)
+				player_speed -= player_speed_difference_amount;
+			else
+				player_speed += player_speed_difference_amount;
 			break;
 		case SpeedStates.DECREASE:
-			DecreaseSpeed ();
+			if (game_speed > game_min_speed["min"])
+				game_speed -= game_speed_diffrence_amount;
+			if (player_speed < player_max_speed["min"])
+				player_speed += player_speed_difference_amount;
 			break;
 		}
-	}
-
-	public void NormalizeSpeed() {
-		if (game_speed > game_normal_speed)
-			game_speed -= game_speed_diffrence_amount;
-		else 
-			game_speed += game_speed_diffrence_amount;
-
-		if (player_speed > player_normal_speed)
-			player_speed -= player_speed_difference_amount;
-		else
-			player_speed += player_speed_difference_amount;
-	}
-
-	public void IncreaseSpeed() {
-		if (game_speed < game_max_speed["min"])
-			game_speed += game_speed_diffrence_amount;
-		if (player_speed > player_min_speed["min"])
-			player_speed -= player_speed_difference_amount;
-	}
-
-	public void DecreaseSpeed() {
-		if (game_speed > game_min_speed["min"])
-			game_speed -= game_speed_diffrence_amount;
-		if (player_speed < player_max_speed["min"])
-			player_speed += player_speed_difference_amount;
 	}
 
 	public void MakeHard() {
