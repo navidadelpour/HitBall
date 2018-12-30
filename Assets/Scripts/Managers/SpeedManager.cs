86ca13d81d9814ed49966082acb445fc8d0f9be1
navidAdelpour <navidadelpour@gmail.com>
2018-12-30 15:09:02 +0330

    cleaning the speedManager

--- Assets/Scripts/Managers/SpeedManager.cs
+++ Assets/Scripts/Managers/SpeedManager.cs
@@ -43,15 +43,16 @@ public class SpeedManager : MonoBehaviour {
 
 	void Update () {
 		MakeHard();
+		bool has_slow_motion = ItemManager.self.has_slow_motion;
 		switch (state) {
 		case SpeedStates.INCREASE:
-			if (game_speed < game_max_speed["min"] / (ItemManager.self.has_slow_motion ? slow_motion_scale : 1f))
+			if (game_speed < game_max_speed["min"] / (has_slow_motion ? slow_motion_scale : 1f))
 				game_speed += game_speed_diffrence_amount;
 			if (player_speed > player_min_speed["min"])
 				player_speed -= player_speed_difference_amount;
 			break;
 		case SpeedStates.NORMALIZE:
-			if (game_speed > game_normal_speed / (ItemManager.self.has_slow_motion ? slow_motion_scale : 1f))
+			if (game_speed > game_normal_speed / (has_slow_motion ? slow_motion_scale : 1f))
 				game_speed -= game_speed_diffrence_amount;
 			else 
 				game_speed += game_speed_diffrence_amount;
@@ -62,7 +63,7 @@ public class SpeedManager : MonoBehaviour {
 				player_speed += player_speed_difference_amount;
 			break;
 		case SpeedStates.DECREASE:
-			if (game_speed > game_min_speed["min"] / (ItemManager.self.has_slow_motion ? slow_motion_scale : 1f))
+			if (game_speed > game_min_speed["min"] / (has_slow_motion ? slow_motion_scale : 1f))
 				game_speed -= game_speed_diffrence_amount;
 			if (player_speed < player_max_speed["min"])
 				player_speed += player_speed_difference_amount;
