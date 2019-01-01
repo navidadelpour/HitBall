ed5139f448de840de6714df040b68ddf63ce880c
navidAdelpour <navidadelpour@gmail.com>
2019-01-01 21:50:26 +0330

    fixing the ground digger bug

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -213,7 +213,8 @@ public class SpawnManager : MonoBehaviour {
 			Quaternion.identity,
 			last_ground.transform
 		);
-		portal_created.GetComponent<Renderer> ().enabled = false;
+		if(ItemManager.self.actives[Item.GROUND_DIGGER])
+			portal_created.GetComponent<Renderer> ().enabled = false;
 		last_item_spawned = Things.PORTAL;
 		has_portal = true;
 	}
