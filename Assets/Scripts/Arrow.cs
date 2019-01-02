273f80e07c019afb39188b62d4e06561de681c64
navidAdelpour <navidadelpour@gmail.com>
2019-01-02 16:48:17 +0330

    arrows have random position and direction

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	private Vector3 direction;
+	private float speed_scale;
+
+	void Awake() {
+		bool b = Random.Range(0, 2) == 0;
+		direction = b ? Vector3.left : Vector3.down;
+		transform.position += b ? Vector3.up * Random.Range(1f, 5f) : Vector3.up * 14 + Vector3.right * Random.Range(1f, 5f);
+		speed_scale = Random.Range(1f, 2f);
+	}
+
+	void Start () {
+	}
+	
+	void Update () {
+
+	}
+}
