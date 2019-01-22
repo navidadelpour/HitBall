e6c8f4f531f89350e0f21b7c10abe889a663498b
navidAdelpour <navidadelpour@gmail.com>
2019-01-22 19:12:40 +0330

    adding onclick for shopitems

---
+++ Assets/Scripts/Controllers/ShopItemButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShopItemButtonController : MonoBehaviour {
+
+	private GameObject clicked_object;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnClick() {
+		Debug.Log(name);
+	}
+}
